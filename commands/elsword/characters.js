7268834b644dcec2b6db6a6415d6800c8f1cf33b
112112112112112 <kimika1863@gmail.com>
2026-02-27 22:53:42 +0100

    add command to display list of characters

---
+++ commands/elsword/characters.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const classes = require('../../classes.js');
+const db = require('../../database.js');
+
+module.exports = {
+	data: new SlashCommandBuilder().setName('characters').setDescription('Display all characters in your account'),
+	async execute(interaction) {
+        const embed = new EmbedBuilder().setTitle(`${interaction.member.displayName}'s Characters`).setColor(Math.floor(Math.random() * 0xFFFFFF));
+        const allCharacters = db.prepare('SELECT name, class FROM characters WHERE user_id = ?').all(interaction.user.id);
+        const allClasses = Object.values(classes).flat();
+        let description = '';
+
+        for (const char of allCharacters) {
+            const emote = allClasses.find(cls => cls.name === char.class).emote;
+            description += `${emote} ${char.name}\n`;
+        };
+
+        embed.setDescription(description);
+        await interaction.reply({ embeds: [embed] });
+	},
+};
