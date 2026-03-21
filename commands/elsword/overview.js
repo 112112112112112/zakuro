9d4f0f3aa02f1e799955d479c83be128f57cceae
112112112112112 <kimika1863@gmail.com>
2026-03-21 20:21:54 +0100

    update overview to always sync checklist

--- commands/elsword/overview.js
+++ commands/elsword/overview.js
@@ -14,23 +14,21 @@ module.exports = {
             return interaction.reply({ content: `Make an account using /register first!`, flags: MessageFlags.Ephemeral});
         }
 
-        const hasChecklist = db.prepare('SELECT * FROM checklist WHERE user_id = ? AND character_id IS NULL').get(interaction.user.id);
-        if (!hasChecklist) {
-            const tasks = db.prepare('SELECT id, bound FROM tasks').all();
-            const chars = db.prepare('SELECT id FROM characters WHERE user_id = ?').all(interaction.user.id);
-            for (const task of tasks) {
-                if (task.bound === 'account') {
-                    const taskExists = db.prepare('SELECT * FROM checklist WHERE user_id = ? AND task_id = ? AND character_id IS NULL').get(interaction.user.id, task.id);
-                    if (!taskExists) {
-                        db.prepare('INSERT INTO checklist (user_id, task_id, completed) VALUES (?, ?, 0)').run(interaction.user.id, task.id);
-                    }
+        // ? Synchronization (updates checklist) ==================================================================
+        const tasks = db.prepare('SELECT id, bound FROM tasks').all();
+        const syncChars = db.prepare('SELECT id FROM characters WHERE user_id = ?').all(interaction.user.id);
+        for (const task of tasks) {
+            if (task.bound === 'account') {
+                const taskExists = db.prepare('SELECT * FROM checklist WHERE user_id = ? AND task_id = ? AND character_id IS NULL').get(interaction.user.id, task.id);
+                if (!taskExists) {
+                    db.prepare('INSERT INTO checklist (user_id, task_id, completed) VALUES (?, ?, 0)').run(interaction.user.id, task.id);
                 }
-                else {
-                    for (const char of chars) {
-                        const taskExists = db.prepare('SELECT * FROM checklist WHERE user_id = ? AND task_id = ? AND character_id = ?').get(interaction.user.id, task.id, char.id);
-                        if (!taskExists) {
-                            db.prepare('INSERT INTO checklist (user_id, character_id, task_id, completed) VALUES (?, ?, ?, 0)').run(interaction.user.id, char.id, task.id);
-                        }
+            }
+            else {
+                for (const char of syncChars) {
+                    const taskExists = db.prepare('SELECT * FROM checklist WHERE user_id = ? AND task_id = ? AND character_id = ?').get(interaction.user.id, task.id, char.id);
+                    if (!taskExists) {
+                        db.prepare('INSERT INTO checklist (user_id, character_id, task_id, completed) VALUES (?, ?, ?, 0)').run(interaction.user.id, char.id, task.id);
                     }
                 }
             }
