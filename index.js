d1f005320802c29301ea9a27394341fdc28f65f6
112112112112112 <kimika1863@gmail.com>
2026-04-09 23:50:23 +0200

    Daily/Weekly reminders with ping

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require(
 const { token } = require('./config.json');
 const cron = require('node-cron');
 const db = require('./database.js');
+const classes = require('./classes.js');
 require('./tasks.js')
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -45,6 +46,26 @@ for (const file of eventFiles) {
 	}
 }
 
+// * Daily reminder
+cron.schedule('0 20 * * *', async () => {
+	const channel = await client.channels.fetch('1426625638483103966');
+	const users = db.prepare('SELECT id FROM users').all();
+
+	for (const user of users) {
+		const missingDailies = db.prepare(`SELECT tasks.title 
+			FROM tasks JOIN checklist ON tasks.id = checklist.task_id 
+			WHERE checklist.user_id = ? AND checklist.completed = 0 AND tasks.reset = 'daily' AND checklist.character_id IS NULL`).all(user.id);
+
+		if (missingDailies.length > 0) {
+			const taskList = missingDailies.map(t => `- ${t.title}`).join('\n');
+            const fullMsg = `<@${user.id}>, don't forget to complete your dailies! Here's what you are missing:\n## Dailies\n${taskList}`
+			await channel.send(fullMsg);
+		}
+	}
+}, {
+	timezone: 'UTC'
+});
+
 // * Daily reset
 cron.schedule('0 0 * * *', async () => {
 	db.prepare("UPDATE checklist SET completed = 0 WHERE task_id IN (SELECT id FROM tasks WHERE reset = 'daily')").run();
@@ -54,18 +75,52 @@ cron.schedule('0 0 * * *', async () => {
 	timezone: 'UTC'
 });
 
-// * Weekly reminder before reset
-cron.schedule('15 16 * * 2', async () => {
-	const weeklies = db.prepare("SELECT title FROM tasks WHERE reset = 'weekly'").all();
-	const weekliesList = weeklies.map(t => `✦ ${t.title}`).join('\n');
+// * Weekly reminder 
+cron.schedule('0 0 * * 2', async () => {
 	const channel = await client.channels.fetch('1426625638483103966');
-	await channel.send(`⚠️ **Weekly tasks will be reset in less than 9 hours!** ⚠️\nRemember to complete them!\n${weekliesList} ⚠️`);
+	const users = db.prepare('SELECT id FROM users').all();
+	const userClasses = Object.values(classes).flat();
+
+	for (const user of users) {
+		    const missingWeeklies = db.prepare(`SELECT tasks.title, characters.name AS char_name, characters.class AS char_class 
+			FROM tasks JOIN checklist ON tasks.id = checklist.task_id 
+			LEFT JOIN characters ON checklist.character_id = characters.id 
+			WHERE checklist.user_id = ? AND checklist.completed = 0 AND tasks.reset = 'weekly'`).all(user.id);
+
+		if (missingWeeklies.length > 0) {
+            const charMap = {};
+            for (const t of missingWeeklies) {
+                if (t.char_name) {
+                    const emote = userClasses.find(cls => cls.name === t.char_class).emote;
+                    const key = `${emote} **${t.char_name}**`;
+                    if (!charMap[key]) {
+                        charMap[key] = [];
+                    }
+                    charMap[key].push(t.title);
+                } else {
+                    if (!charMap['account']) {
+                        charMap['account'] = [];
+                    }
+                    charMap['account'].push(t.title);
+                }
+            }
+
+            const taskList = Object.entries(charMap).map(([char, tasks]) => {
+                const header = char === 'account' ? '' : char;
+                return `${header}\n${tasks.map(t => `- ${t}`).join('\n')}`;
+            }).join('\n\n');
+
+            const fullMsg = `<@${user.id}>, don't forget to complete your weeklies! Here's what you are missing:\n## Weeklies\n${taskList}`
+            const msg = fullMsg.length > 2000 ? fullMsg.substring(0, 1997) + '...' : fullMsg;
+			await channel.send(msg);
+		}
+	}
 }, {
 	timezone: 'UTC'
 });
 
 // * Weekly reset
-cron.schedule('0 0 * * 3', async () => {
+cron.schedule('0 10 * * 3', async () => {
 	db.prepare("UPDATE checklist SET completed = 0 WHERE task_id IN (SELECT id FROM tasks WHERE reset = 'weekly')").run();
 	const channel = await client.channels.fetch('1426625638483103966');
 	await channel.send('Weekly tasks have been reset!');
