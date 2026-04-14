dad6ef8a260182831f028c1f52c7c9ac67e02154
112112112112112 <kimika1863@gmail.com>
2026-04-14 18:48:15 +0200

    add changelog command

---
+++ commands/elsword/changelog.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { execSync } = require('child_process');
+
+module.exports = {
+	data: new SlashCommandBuilder().setName('changelog').setDescription('Display latest bot updates'),
+	async execute(interaction) {
+        // ? (YYYY-MM-DD) Commit message
+        const log = execSync('git log -10 --no-merges --pretty=format:"(%ad) %s" --date=short').toString();
+
+        await interaction.reply('Here are the latest bot updates: ', log);
+	},
+};
